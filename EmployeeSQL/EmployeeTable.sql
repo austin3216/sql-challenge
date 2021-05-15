257cc9915539adb4da69e82c6ad1c753b8699a7c
austin3216 <aroyster@pm.me>
2021-05-15 16:26:09 -0600

    updated erd and schema

--- EmployeeSQL/EmployeeTable.sql
+++ EmployeeSQL/EmployeeTable.sql
@@ -5,56 +5,44 @@
 -- HP Employees
 
 CREATE TABLE "Departments" (
-    "dept_no" varchar(10)   NOT NULL,
-    "dept_name" varchar(30)   NOT NULL,
+    "dept_no" varchar   NOT NULL,
+    "dept_name" varchar   NOT NULL,
     CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "dept_no"
-     )
-);
-
-CREATE TABLE "Dept_Emp" (
-    "dept_no" varchar(10)   NOT NULL,
-    "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_Dept_Emp" PRIMARY KEY (
-        "dept_no","emp_no"
-     )
-);
-
-CREATE TABLE "Dept_Manager" (
-    "dept_no" varchar(10)   NOT NULL,
-    "emp_no" int   NOT NULL,
-    CONSTRAINT "pk_Dept_Manager" PRIMARY KEY (
         "dept_no"
      )
 );
 
 CREATE TABLE "Employees" (
     "emp_no" int   NOT NULL,
-    "emp_title_id" varchar(5)   NOT NULL,
+    "emp_title_id" varchar   NOT NULL,
     "birth_date" date   NOT NULL,
-    "first_name" varchar(30)   NOT NULL,
-    "last_name" varchar(30)   NOT NULL,
-    "sex" varchar(1)   NOT NULL,
+    "first_name" varchar   NOT NULL,
+    "last_name" varchar   NOT NULL,
+    "sex" varchar   NOT NULL,
     "hire_date" date   NOT NULL,
     CONSTRAINT "pk_Employees" PRIMARY KEY (
         "emp_no"
      )
 );
 
+CREATE TABLE "Dept_Emp" (
+    "dept_no" varchar   NOT NULL,
+    "emp_no" int   NOT NULL
+);
+
+CREATE TABLE "Dept_Manager" (
+    "dept_no" varchar   NOT NULL,
+    "emp_no" int   NOT NULL
+);
+
 CREATE TABLE "Salaries" (
     "emp_no" int   NOT NULL,
-    "salary" int   NOT NULL,
-    CONSTRAINT "pk_Salaries" PRIMARY KEY (
-        "emp_no"
-     )
+    "salary" int   NOT NULL
 );
 
 CREATE TABLE "Titles" (
-    "title_id" varchar(5)   NOT NULL,
-    "title" varchar(50)   NOT NULL,
-    CONSTRAINT "pk_Titles" PRIMARY KEY (
-        "title_id"
-     )
+    "title_id" varchar   NOT NULL,
+    "title" varchar   NOT NULL
 );
 
 ALTER TABLE "Dept_Emp" ADD CONSTRAINT "fk_Dept_Emp_dept_no" FOREIGN KEY("dept_no")
@@ -69,9 +57,9 @@ REFERENCES "Departments" ("dept_no");
 ALTER TABLE "Dept_Manager" ADD CONSTRAINT "fk_Dept_Manager_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
 
-ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
-REFERENCES "Titles" ("title_id");
-
 ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_title_id" FOREIGN KEY("title_id")
+REFERENCES "Employees" ("emp_title_id");
 
